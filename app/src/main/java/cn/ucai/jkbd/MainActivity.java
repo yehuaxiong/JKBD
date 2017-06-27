c218df8b7d387831cef8a28547a9a6f699a5cc8c
yehuaxiong <yehuaxiong1017@qq.com>
2017-06-27 22:16:00 +0800

    first commit

---
+++ app/src/main/java/cn/ucai/jkbd/MainActivity.java
@@ -0,0 +1,22 @@
+package cn.ucai.jkbd;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent=new Intent();
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+}
---
+++ app/src/main/java/cn/ucai/jkbd/SplashActivity.java
@@ -0,0 +1,47 @@
+package cn.ucai.jkbd;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by clawpo on 2017/6/27.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        mCountDownTimer.start();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    finish();
+                }
+                break;
+            default:
+        }
+    }
+
+
+    CountDownTimer mCountDownTimer  = new CountDownTimer(2000,1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+        }
+
+        @Override
+        public void onFinish() {
+            Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+            //startActivity(intent);
+            startActivityForResult(intent, 1);
+        }
+    };
+}
