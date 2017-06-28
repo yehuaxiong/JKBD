dbe643adfe025465a65b4d09fafedd0dacc38fa8
yehuaxiong <yehuaxiong1017@qq.com>
2017-06-28 22:16:59 +0800

    重新修改obj类实体类

--- app/src/main/java/cn/ucai/jkbd/bean/obj.java
+++ app/src/main/java/cn/ucai/jkbd/bean/obj.java
@@ -7,7 +7,26 @@ import java.util.List;
  */
 
 public class obj {
+    private int error_code;
+    private String reason;
     private List<exam> exams;
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public int getError_code() {
+        return error_code;
+    }
+
+    public void setError_code(int error_code) {
+        this.error_code = error_code;
+    }
+
 
     public List<exam> getExams() {
         return exams;
