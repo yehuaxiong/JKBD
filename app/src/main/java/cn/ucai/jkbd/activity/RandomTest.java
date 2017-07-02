be012ff015204d9b8415065839530ed356dd3efe
yehuaxiong <yehuaxiong1017@qq.com>
2017-07-02 11:17:25 +0800

    添加 Picasso下载网络图片

--- app/src/main/java/cn/ucai/jkbd/activity/RandomTest.java
+++ app/src/main/java/cn/ucai/jkbd/activity/RandomTest.java
@@ -3,7 +3,10 @@ package cn.ucai.jkbd.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -20,6 +23,7 @@ public class RandomTest extends Activity {
     private TextView subject,limitTime,questionCount;
 
     private TextView tv_id,tv_question,q1,q2,q3,q4;
+    private ImageButton img_pic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +45,7 @@ public class RandomTest extends Activity {
         q2= (TextView) findViewById(R.id.tv_B);
         q3= (TextView) findViewById(R.id.tv_C);
         q4= (TextView) findViewById(R.id.tv_D);
+        img_pic= (ImageButton) findViewById(R.id.img_pic);
 
 
 //        subject.setText(""+exam.getSubjectTitle());
@@ -57,6 +62,7 @@ public class RandomTest extends Activity {
         q2.setText(""+questionList.get(0).getItem2());
         q3.setText(""+questionList.get(0).getItem3());
         q4.setText(""+questionList.get(0).getItem4());
+        Picasso.with(this).load(questionList.get(0).getUrl()).into(img_pic);
 
     }
 }
