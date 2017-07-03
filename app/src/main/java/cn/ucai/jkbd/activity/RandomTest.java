5325f6836602c5d3b969bab09c44a9e2493ef2bd
yehuaxiong <yehuaxiong1017@qq.com>
2017-07-04 00:45:38 +0800

    将CheckBox 改成RadioButton

--- app/src/main/java/cn/ucai/jkbd/activity/RandomTest.java
+++ app/src/main/java/cn/ucai/jkbd/activity/RandomTest.java
@@ -12,6 +12,7 @@ import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -35,7 +36,7 @@ public class RandomTest extends Activity {
     private ImageButton img_pic;
     private LinearLayout linearLayout,ll_C,ll_D;
     private ProgressBar pb_loading;
-    private CheckBox cb_C,cb_D;
+    private RadioButton cb_A,cb_B,cb_C,cb_D;
 
     Exam exam;
     Question question;
@@ -168,18 +169,30 @@ public class RandomTest extends Activity {
 
         ll_C= (LinearLayout) findViewById(R.id.ll_C);
         ll_D= (LinearLayout) findViewById(R.id.ll_D);
-        cb_C= (CheckBox) findViewById(R.id.cb_C);
-        cb_D= (CheckBox) findViewById(R.id.cb_D);
+
+        cb_A= (RadioButton) findViewById(R.id.cb_A);
+        cb_B= (RadioButton) findViewById(R.id.cb_B);
+        cb_C= (RadioButton) findViewById(R.id.cb_C);
+        cb_D= (RadioButton) findViewById(R.id.cb_D);
     }
 
     public void pre(View view) {
+        //this.initRadioButton();
         iExamBiz.Pre();
         initData();
     }
 
     public void next(View view) {
+        //this.initRadioButton();
         iExamBiz.Next();
         initData();
+    }
+
+    public void initRadioButton(){
+        cb_A.setChecked(false);
+        cb_B.setChecked(false);
+        cb_C.setChecked(false);
+        cb_D.setChecked(false);
     }
 
     class LoadExamBroadcast extends BroadcastReceiver{
