2e781b46cbc066f8bfc00ad6e8ac5bbd5339ff83
yehuaxiong <yehuaxiong1017@qq.com>
2017-07-04 00:12:23 +0800

    考试界面中有判断题的答案只显示A，B选项和答案

--- app/src/main/java/cn/ucai/jkbd/activity/RandomTest.java
+++ app/src/main/java/cn/ucai/jkbd/activity/RandomTest.java
@@ -8,6 +8,7 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
@@ -32,8 +33,9 @@ public class RandomTest extends Activity {
     private TextView subject,limitTime,questionCount,tv_loading;
     private TextView tv_id,tv_question,q1,q2,q3,q4;
     private ImageButton img_pic;
-    private LinearLayout linearLayout;
+    private LinearLayout linearLayout,ll_C,ll_D;
     private ProgressBar pb_loading;
+    private CheckBox cb_C,cb_D;
 
     Exam exam;
     Question question;
@@ -107,7 +109,6 @@ public class RandomTest extends Activity {
                     Log.e("exam","exam为空！");
                 }
 
-//                questionList=ExamApplication.getInstance().getQuestionList();
                 question=iExamBiz.getQuestion();
                 if(question!=null){
                     tv_id.setText(iExamBiz.getIndex()+1+".");
@@ -116,6 +117,12 @@ public class RandomTest extends Activity {
                     q2.setText(""+question.getItem2());
                     q3.setText(""+question.getItem3());
                     q4.setText(""+question.getItem4());
+
+                    ll_C.setVisibility(question.getItem3().equals("")?View.GONE:View.VISIBLE);
+                    ll_D.setVisibility(question.getItem4().equals("")?View.GONE:View.VISIBLE);
+                    cb_C.setVisibility(question.getItem3().equals("")?View.GONE:View.VISIBLE);
+                    cb_D.setVisibility(question.getItem4().equals("")?View.GONE:View.VISIBLE);
+
                     if(question.getUrl()!=null&&!question.getUrl().equals("")){
                         img_pic.setVisibility(View.VISIBLE);
                         Picasso.with(this).load(question.getUrl()).into(img_pic);
@@ -158,6 +165,11 @@ public class RandomTest extends Activity {
         linearLayout= (LinearLayout) findViewById(R.id.ll_loading);
         tv_loading= (TextView) findViewById(R.id.tv_loading);
         pb_loading= (ProgressBar) findViewById(R.id.pb_loading);
+
+        ll_C= (LinearLayout) findViewById(R.id.ll_C);
+        ll_D= (LinearLayout) findViewById(R.id.ll_D);
+        cb_C= (CheckBox) findViewById(R.id.cb_C);
+        cb_D= (CheckBox) findViewById(R.id.cb_D);
     }
 
     public void pre(View view) {
