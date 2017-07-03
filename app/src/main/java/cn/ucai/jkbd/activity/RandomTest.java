54af41e71cc422571ee8f9a831584cf29de2e31b
yehuaxiong <yehuaxiong1017@qq.com>
2017-07-03 20:28:40 +0800

    完善 下载考题数据的 加载页面

--- app/src/main/java/cn/ucai/jkbd/activity/RandomTest.java
+++ app/src/main/java/cn/ucai/jkbd/activity/RandomTest.java
@@ -60,7 +60,8 @@ public class RandomTest extends Activity {
 
         initView();
 
-        loadDate();
+        iExamBiz=new ExamBiz();
+        loadData();
     }
 
     @Override
@@ -79,8 +80,10 @@ public class RandomTest extends Activity {
         registerReceiver(loadQuestionBroadcast,new IntentFilter(ExamApplication.LOAD_QUESTION));
     }
 
-    private  void loadDate(){
-        iExamBiz=new ExamBiz();
+    private  void loadData(){
+        linearLayout.setEnabled(false);
+        pb_loading.setVisibility(View.VISIBLE);
+        tv_loading.setText("Loading...");
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -116,8 +119,16 @@ public class RandomTest extends Activity {
                     Log.e("onCreate:question","questionList为空！");
                 }
             }else{
+                linearLayout.setEnabled(true);
                 pb_loading.setVisibility(View.GONE);
                 tv_loading.setText("加载失败，点击屏幕重新加载！");
+
+                linearLayout.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        loadData();
+                    }
+                });
             }
         }
 
